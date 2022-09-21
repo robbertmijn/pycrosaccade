1bfe3d5b4e352a20b3afde1a6d8946c738721e0f
robbertmijn <robbertmijn@gmail.com>
2022-09-21 13:47:36 +0200

    rename

--- micSaccer/__init__.py
+++
@@ -1,3 +0,0 @@
-__version__ = '0.1.0'
-
-from micSaccer._micSaccer import find_microsaccades, microsaccades
---
+++ pycrosaccade/__init__.py
@@ -0,0 +1,3 @@
+__version__ = '0.2.0'
+
+from pycrosaccade._pycrosaccade import find_microsaccades, microsaccades
