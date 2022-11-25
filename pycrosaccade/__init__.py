bd86129797d652047f5f20e91e484a6289f7d37a
robbertmijn <robbertmijn@gmail.com>
2022-11-25 11:02:05 +0100

    Update __init__.py
    missed that _

--- pycrosaccade/__init__.py
+++ pycrosaccade/__init__.py
@@ -1,5 +1,5 @@
 """Detect microsaccades"""
 
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 
-from pycrosaccade._pycrosaccade import find_microsaccades, microsaccades
+from pycrosaccade._pycrosaccade import _find_microsaccades, microsaccades
