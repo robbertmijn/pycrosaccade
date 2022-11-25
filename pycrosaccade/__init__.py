62c96202bd0c8f94a1f047d708680b514a3e9abc
robbertmijn <robbertmijn@gmail.com>
2022-11-25 19:04:38 +0100

    Update __init__.py

--- pycrosaccade/__init__.py
+++ pycrosaccade/__init__.py
@@ -1,5 +1,5 @@
 """Detect microsaccades"""
 
-__version__ = '0.4.0'
+__version__ = '0.4.1'
 
-from pycrosaccade._pycrosaccade import _find_microsaccades, microsaccades, round_to_odd
+from pycrosaccade._pycrosaccade import _find_microsaccades, microsaccades, _round_to_odd, plot_dist_dur
